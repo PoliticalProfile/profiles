b17c62defb2da2df3ee20c5fe293b6de4f973835
Henrique Nogueira <hrqnogueira@gmail.com>
2018-08-05 18:00:06 -0300

    Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn political_profile.wsgi --log-file -
--- political_profile/settings/prod.py
+++ political_profile/settings/prod.py
@@ -1,1 +1,4 @@
 from political_profile.settings.base import *
+
+STATIC_URL = 'https://political-profile.nyc3.digitaloceanspaces.com/'
+
